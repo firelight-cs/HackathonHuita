e5f4231e1761d560becb88614980f8008d6a155f
Ruslan Kildibekov <awitkild28.08@gmail.com>
2025-05-24 13:21:14 +0200

    per product implementation

--- secondary.py
+++ secondary.py
@@ -1,1 +1,5 @@
-#asd
+import pandas as pd
+
+sell_df = pd.read_csv('./data/sell_data_cleaned.csv', sep=';')
+
+print(sell_df.groupby('Product').count().sort_values('Quantity', ascending=False).head(10))
